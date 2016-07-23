0173e688eb9f06108c9f7540a2a4d807712004e8
Alexander Ivantsov <alexivantsov@ya.ru>
2016-07-23 15:05:56 +0200

    add icons for header

---
+++ src/pages/popup/components/header/icons/compose.js
@@ -0,0 +1,9 @@
+import React from 'react';
+
+const ComposeIcon = (props) => (
+    <svg  viewBox="0 0 18 18" {...props}>
+        <path d="M5.4 13.6l3.7-1.4 8.4-8.4c.6-.6.6-1.7 0-2.3s-1.7-.6-2.3 0l-8.4 8.4-1.4 3.7zm10.6 2.4h-14v-14h6.5l2-2h-10.5v18h18v-9.6l-2 2v5.6z"/>
+    </svg>
+);
+
+export default ComposeIcon;
