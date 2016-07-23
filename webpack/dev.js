152b6933e49a363d6499cd88ba58202dcda12d15
Alexander Ivantsov <alexivantsov@ya.ru>
2016-07-23 15:03:12 +0200

    use less

--- webpack/dev.js
+++ webpack/dev.js
@@ -23,10 +23,11 @@ module.exports = {
             test: /\.js$/,
             loader: 'babel'
         }, {
-            test: /\.css$/,
+            test: /\.less/,
             loaders: [
-                'style-loader',
-                'css-loader?modules&importLoaders=1&localIdentName=[name]__[local]___[hash:base64:5]'
+                'style',
+                'css?modules&importLoaders=1&localIdentName=[name]__[local]___[hash:base64:5]',
+                'less'
             ]
         }]
     },
