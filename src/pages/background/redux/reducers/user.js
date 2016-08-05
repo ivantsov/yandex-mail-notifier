3524b31b9b23ff6612f42be24fc0d01afeeca20a
Alexander Ivantsov <alexivantsov@ya.ru>
2016-08-05 23:22:13 +0200

    add user reducer

---
+++ src/pages/background/redux/reducers/user.js
@@ -0,0 +1,19 @@
+import {LOGIN} from '../constants/user';
+
+const initialState = {
+    authorized: false,
+    email: null
+};
+
+export default function (state = initialState, action) {
+    switch (action.type) {
+        case LOGIN:
+            return {
+                ...state,
+                authorized: true,
+                email: action.data || state.email // in case when we already have email
+            };
+        default:
+            return state;
+    }
+}
