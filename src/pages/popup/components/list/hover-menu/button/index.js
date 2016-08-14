bc9cc3024a39c91c8c8d43fd3ff7e79bedfa443e
Alexander Ivantsov <alexivantsov@ya.ru>
2016-08-14 11:35:02 +0200

    add button component in hover menu

---
+++ src/pages/popup/components/list/hover-menu/button/index.js
@@ -0,0 +1,45 @@
+import styles from './button.less';
+
+import React, {PropTypes, Component} from 'react';
+import types from './types';
+
+export default class Button extends Component {
+    static propTypes = {
+        id: PropTypes.string.isRequired,
+        type: PropTypes.string.isRequired,
+        onClick: PropTypes.func.isRequired
+    };
+
+    render() {
+        const {Icon, text} = types[this.props.type];
+
+        return (
+            <a className={styles.component} onClick={this.onClick}>
+                <Icon className={styles.icon}/>
+                <div>{text}</div>
+            </a>
+        );
+    }
+
+    onClick = () => {
+        const {
+            id,
+            type,
+            onClick
+        } = this.props;
+        const typeObj = types[type];
+
+        // for open message action
+        if (typeObj.action) {
+            return typeObj.action(id);
+        }
+
+        // for the rest actions
+        onClick({
+            id,
+            oper: typeObj.actionType
+        });
+    }
+}
+
+export {types};
