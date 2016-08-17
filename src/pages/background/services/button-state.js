ad4721bb84488bc2e31b220cbe9b05f0a654e082
Alexander Ivantsov <alexivantsov@ya.ru>
2016-08-17 22:05:50 +0200

    add button-state service

---
+++ src/pages/background/services/button-state.js
@@ -0,0 +1,22 @@
+import userSubscribe from '../redux/subscribers/user';
+import messagesSubscribe from '../redux/subscribers/messages';
+
+const popup = chrome.browserAction;
+
+function setState(enabled, badge) {
+    // enable or disable popup button
+    enabled ? popup.enable() : popup.disable();
+
+    // set badge
+    popup.setBadgeText({text: (badge || '').toString()});
+    popup.setBadgeBackgroundColor({color: '#3d5afe'});
+}
+
+export default function () {
+    setState(false);
+
+    userSubscribe('login', ({messages}) => setState(true, messages.unreadCount));
+    userSubscribe('logout', ({messages}) => setState(false, messages.unreadCount));
+
+    messagesSubscribe('unreadCountChanged', state => setState(state.user.authorized, state.messages.unreadCount));
+}
