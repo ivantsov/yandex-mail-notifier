44d900194d583def5bc6c800febd7a0607f850dd
Alexander Ivantsov <alexivantsov@ya.ru>
2016-08-20 23:23:40 +0200

    add client for websocket service

---
+++ src/pages/background/services/websocket/client.js
@@ -0,0 +1,71 @@
+import qs from 'query-string';
+import {RECONNECT, NEW_MESSAGE} from './constants';
+
+const WS_URL = 'wss://xiva-daria.mail.yandex.net/events/websocket';
+
+let ws, emitEvent;
+
+function onOpen(...args) {
+    console.log('Socket is opened', args); // eslint-disable-line no-console
+}
+
+function onClose(...args) {
+    console.error('Socket is closed', args); // eslint-disable-line no-console
+    emitEvent(RECONNECT);
+}
+
+function onError(...args) {
+    console.error('An error has occurred in socket', args); // eslint-disable-line no-console
+    emitEvent(RECONNECT);
+}
+
+function onMessage({data}) {
+    const {operation, message} = JSON.parse(data);
+
+    if (operation !== 'ping') {
+        emitEvent(NEW_MESSAGE, message);
+    }
+}
+
+function connect(data) {
+    const {
+        sign,
+        ts,
+        uid
+    } = data;
+    const queryParams = qs.stringify({
+        client_id: 'bar',
+        service: 'mail',
+        format: 'json',
+        uid,
+        sign,
+        ts
+    });
+
+    ws = new WebSocket(`${WS_URL}?${queryParams}`);
+
+    ws.addEventListener('open', onOpen, false);
+    ws.addEventListener('close', onClose, false);
+    ws.addEventListener('message', onMessage, false);
+    ws.addEventListener('error', onError, false);
+}
+
+function disconnect() {
+    if (ws) {
+        ws.close();
+
+        ws.removeEventListener('open', onOpen, false);
+        ws.removeEventListener('close', onClose, false);
+        ws.removeEventListener('message', onMessage, false);
+        ws.removeEventListener('error', onError, false);
+    }
+}
+
+export default function (ev) {
+    emitEvent = ev;
+
+    return {
+        connect,
+        disconnect
+    };
+};
