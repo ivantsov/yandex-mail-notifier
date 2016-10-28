7288d38b2d234453cab86b52a90b9885c89babfc
Alexander Ivantsov <alexivantsov@ya.ru>
2016-10-28 12:32:20 +0200

    add settings page entrypoint

---
+++ src/pages/settings/index.js
@@ -0,0 +1,24 @@
+// import './styles/layout.less';
+
+// TODO: fix styles
+import React from 'react';
+import ReactDOM from 'react-dom';
+import {Provider} from 'react-redux';
+import {createUIStore} from 'redux-webext';
+import App from './components/app';
+
+async function initApp() {
+    const store = await createUIStore();
+
+    const mountNode = document.createElement('div');
+    document.body.appendChild(mountNode);
+
+    ReactDOM.render(
+        <Provider store={store}>
+            <App/>
+        </Provider>,
+        mountNode
+    );
+}
+
+initApp();
