684dd823049c673fcbbb79bb8c490a7389d2edbd
etsatsina <e.tsatsina@ya.ru>
2016-10-30 14:20:08 +0100

    open yandex page on button click if user not authrorized

--- src/pages/background/services/button-state.js
+++ src/pages/background/services/button-state.js
@@ -1,11 +1,20 @@
+import openTab from 'shared/utils/tab';
 import userSubscribe from '../redux/subscribers/user';
 import messagesSubscribe from '../redux/subscribers/messages';
 
 const popup = chrome.browserAction;
 
 function setState(enabled, badge) {
-    // enable or disable popup button
-    enabled ? popup.enable() : popup.disable();
+    // enable or disable popup button (onClick works only if no popup set)
+    if (enabled) {
+        popup.setPopup({popup: 'pages/popup.html'});
+        // popup.enable();
+    }
+    else {
+        popup.setPopup({popup: ''});
+        // TODO: change icon instead of disabling otherwise onClick doesn't work
+        // popup.disable();
+    }
 
     // set badge
     popup.setBadgeText({text: (badge || '').toString()});
@@ -13,6 +22,8 @@ function setState(enabled, badge) {
 }
 
 export default function () {
+    popup.onClicked.addListener(openTab);
+
     userSubscribe('login', ({messages}) => setState(true, messages.unreadCount));
     userSubscribe('logout', ({messages}) => setState(false, messages.unreadCount));
 
