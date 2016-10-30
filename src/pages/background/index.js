b69178f4fb472753a153edcb42e7b3643a60b902
Alexander Ivantsov <alexivantsov@ya.ru>
2016-10-30 18:28:05 +0100

    add background entry point

---
+++ src/pages/background/index.js
@@ -0,0 +1,24 @@
+import store from './redux/store';
+import {login, logout} from './redux/actions/user';
+import {loadSettings} from './redux/actions/settings';
+import {initCookieListener, getSessionId} from './services/cookie';
+import initWS from './services/websocket';
+import initButtonState from './services/button-state';
+import initNotification from './services/notification';
+
+async function initApp() {
+    const {dispatch} = store;
+    const isAuth = await getSessionId();
+
+    // we should load settings before starting notification service and login/logout actions
+    await dispatch(loadSettings());
+    initNotification();
+
+    dispatch(isAuth ? login() : logout());
+}
+
+initCookieListener();
+initWS();
+initButtonState();
+
+initApp();
