a1a58c78573b091cbbe54bbb1761c285cf25feaf
Alexander Ivantsov <alexivantsov@ya.ru>
2016-12-10 18:12:56 +0100

    add raven module

---
+++ src/pages/raven.js
@@ -0,0 +1,10 @@
+import Raven from 'raven-js';
+import config from '../manifest.json';
+
+Raven.config('https://a4bc742caec34db89cb376ce5d9c049d@sentry.io/120928', {
+    release: config.version
+}).install();
+
+window.onunhandledrejection = (err) => {
+    Raven.captureException(err.reason);
+};
