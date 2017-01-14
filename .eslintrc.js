524fb7140b927329465ba8c2e59127a3ff3a8ff8
Alexander Ivantsov <alexivantsov@ya.ru>
2017-01-14 13:50:59 +0100

    update eslint config and packages

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,5 +1,59 @@
 const INDENT = 4;
 
+const base = {
+    indent: ['error', INDENT, {
+        SwitchCase: 1
+    }],
+    'comma-dangle': ['error', 'never'],
+    'brace-style': ['error', 'stroustrup', {
+        'allowSingleLine': false
+    }],
+    'object-curly-spacing': ['error', 'never'],
+    'max-len': 'off',
+    'one-var': ['error', {
+        initialized: 'never'
+    }],
+    'one-var-declaration-per-line': ['error', 'initializations'],
+    'arrow-parens': 'off',
+    'arrow-body-style': 'off',
+    'no-prototype-builtins': 'off'
+};
+
+const react = {
+    'react/jsx-filename-extension': ['error', {extensions: ['.js']}],
+    'react/jsx-indent': ['error', INDENT],
+    'react/jsx-indent-props': ['error', INDENT],
+    'react/jsx-space-before-closing': ['error', 'never'],
+    'react/jsx-tag-spacing': ['error', {
+        closingSlash: 'never',
+        beforeSelfClosing: 'never',
+        afterOpening: 'never'
+    }],
+    'react/forbid-prop-types': 'off',
+    'react/no-unused-prop-types': ['error', {
+        skipShapeProps: true
+    }],
+    'react/no-array-index-key': 'off',
+    'react/sort-comp': ['error', {
+        order: [
+            'static-methods',
+            'lifecycle',
+            'rendering',
+            'everything-else'
+        ],
+        groups: {
+            rendering: [
+                'render',
+                '/^render.+$/'
+            ]
+        }
+    }],
+};
+
+const other = {
+    'jsx-a11y/no-static-element-interactions': 'off'
+};
+
 module.exports = {
     parser: 'babel-eslint',
     extends: 'airbnb',
@@ -18,52 +72,9 @@ module.exports = {
             }
         }
     },
-    rules: {
-        indent: ['error', INDENT, {
-            SwitchCase: 1
-        }],
-        'linebreak-style': 'off',
-        'comma-dangle': ['error', 'never'],
-        'brace-style': ['error', 'stroustrup', {
-            'allowSingleLine': false
-        }],
-        'object-curly-spacing': ['error', 'never'],
-        'no-unused-expressions': ['error', {
-            allowShortCircuit: true,
-            allowTernary: true
-        }],
-        'max-len': 'off',
-        'no-param-reassign': 'off',
-        'one-var': ['error', {
-            initialized: 'never'
-        }],
-        'one-var-declaration-per-line': ['error', 'initializations'],
-        'arrow-parens': 'off',
-        'arrow-body-style': 'off',
-        'no-prototype-builtins': 'off',
-
-        'react/jsx-filename-extension': ['error', {extensions: ['.js']}],
-        'react/jsx-indent': ['error', INDENT],
-        'react/jsx-indent-props': ['error', INDENT],
-        'react/jsx-space-before-closing': ['error', 'never'],
-        'react/forbid-prop-types': 'off',
-        'react/no-unused-prop-types': ['error', {
-            skipShapeProps: true
-        }],
-        'react/sort-comp': ['error', {
-            order: [
-                'static-methods',
-                'lifecycle',
-                '/^render.+$/',
-                'render',
-                '/^on.+$/',
-                '/^(get|set)(?!(InitialState$|DefaultProps$|ChildContext$)).+$/',
-                'everything-else'
-            ],
-        }],
-
-
-        'import/prefer-default-export': 'off',
-        'jsx-a11y/no-static-element-interactions': 'off'
-    }
+    rules: Object.assign({},
+        base,
+        react,
+        other
+    )
 };
