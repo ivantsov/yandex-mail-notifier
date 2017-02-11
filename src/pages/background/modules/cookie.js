bdb6b7fe6a797c16edeb5b2db8d2d79bd1de3a8d
Alexander Ivantsov <alexivantsov@ya.ru>
2017-02-11 11:48:11 +0100

    use only “.ru” domain for cookie

--- src/pages/background/modules/cookie.js
+++ src/pages/background/modules/cookie.js
@@ -2,8 +2,7 @@ import store from '../redux/store';
 import {login, logout} from '../redux/actions/user';
 
 const config = {
-    domain: 'yandex',
-    preferredDomain: 'yandex.ru',
+    domain: '.yandex.ru',
     path: '/',
     items: {
         sessionId: 'Session_id',
@@ -16,26 +15,14 @@ let currentDomain;
 function getCookieByName(name) {
     return new Promise(resolve => {
         chrome.cookies.getAll({
+            domain: config.domain,
             path: config.path,
             name
-        }, cookies => {
-            let cookieValue;
-
-            if (cookies && Array.isArray(cookies)) {
-                const availableCookies = cookies.filter(({domain, value}) =>
-                    domain.includes(currentDomain || config.domain) &&
-                    !value.includes('noauth')
-                );
-
-                if (availableCookies.length) {
-                    const cookie = availableCookies.find(({domain}) => domain.includes(config.preferredDomain)) || availableCookies[0];
-                    currentDomain = cookie.domain;
-                    cookieValue = cookie.value;
-                }
-            }
-
-            resolve(cookieValue);
-        });
+        }, res => resolve(
+            Array.isArray(res) &&
+            res[0] &&
+            res[0].value
+        ));
     });
 }
 
@@ -47,32 +34,19 @@ export function getSessionId() {
     return getCookieByName(config.items.sessionId);
 }
 
-export function getCurrentDomain() {
-    return currentDomain;
-}
-
 export function initCookieListener() {
     chrome.cookies.onChanged.addListener(({cookie, removed}) => {
         const {
             domain,
             name,
-            path,
-            value
+            path
         } = cookie;
 
         if (domain.includes(currentDomain || config.domain) &&
             name === config.items.sessionId &&
             path === config.path
         ) {
-            if (removed || value.includes('noauth')) {
-                currentDomain = null;
-                store.dispatch(logout());
-            }
-            else {
-                // if user logged in via another domain we should update it
-                currentDomain = domain;
-                store.dispatch(login());
-            }
+            store.dispatch(removed ? logout() : login());
         }
     });
 }
