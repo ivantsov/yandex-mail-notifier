c53f3419681554d3d0161d22e09484d21f7f5f4a
Alexander Ivantsov <alexivantsov@ya.ru>
2017-05-16 23:28:02 +0200

    Add button component

---
+++ src/pages/popup/components/Button/Button.js
@@ -0,0 +1,40 @@
+import React, {PropTypes} from 'react';
+
+import styles from './Button.less';
+
+const TYPES = {
+    primary: {
+        name: 'primary',
+        className: styles.primary,
+    },
+    secondary: {
+        name: 'secondary',
+        className: styles.secondary,
+    },
+    notButton: {
+        name: 'notButton',
+        className: styles.notBtn
+    }
+};
+
+const Button = ({
+    children,
+    onClick,
+    type,
+}) => (
+    <button
+        className={TYPES[type].className}
+        onClick={onClick}
+    >{children}</button>
+);
+
+Button.propTypes = {
+    children: PropTypes.any.isRequired,
+    onClick: PropTypes.func.isRequired,
+    type: PropTypes.oneOf(Object.keys(TYPES))
+};
+Button.defaultProps = {
+    type: TYPES.primary.name
+};
+
+export default Button;
