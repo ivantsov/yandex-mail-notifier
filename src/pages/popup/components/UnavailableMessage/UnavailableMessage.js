37066835229a64266dfbf691fea0f24af5df772f
Alexander Ivantsov <alexivantsov@ya.ru>
2017-05-16 23:27:11 +0200

    Refactoring unavailable-message component

---
+++ src/pages/popup/components/UnavailableMessage/UnavailableMessage.js
@@ -0,0 +1,24 @@
+import React, {PropTypes} from 'react';
+import Translation from '../Translation';
+import Button from '../Button/Button';
+
+import styles from './UnavailableMessage.less';
+
+const UnavailableMessage = ({reloadApp,}) => (
+    <div className={styles.container}>
+        <p><Translation id="notAuthorized.title"/></p>
+        <p><Translation id="notAuthorized.subTitle"/></p>
+
+        <div className={styles.btnContainer}>
+            <Button onClick={reloadApp}>
+                <Translation id="notAuthorized.btnText"/>
+            </Button>
+        </div>
+    </div>
+);
+
+UnavailableMessage.propTypes = {
+    reloadApp: PropTypes.func.isRequired,
+};
+
+export default UnavailableMessage;
