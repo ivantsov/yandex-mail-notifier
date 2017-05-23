81738fb2c8e7064c019de2ed90cc71b5b099bc5b
Alexander Ivantsov <alexivantsov@ya.ru>
2017-05-23 19:47:56 +0200

    Fix connecting to websocket (#32)

--- src/pages/background/utils/api.js
+++ src/pages/background/utils/api.js
@@ -1,10 +1,16 @@
 import request from 'superagent';
 import errors from 'shared/errors';
-import {getUid} from '../modules/cookie';
+import {getUid as getCookieUid, getSessionId} from '../modules/cookie';
 import parseXML from './parser';
 
 const DOMAIN = 'https://mail.yandex.ru';
 const API_URL = `${DOMAIN}/api`;
+const AUTH_CONFIG = {
+    tokenUrl: 'https://oauth.yandex.ru/token',
+    passportUrl: 'https://pass.yandex.ru/accounts',
+    clientId: '49c545918c574ac28dd7d27e8297065a',
+    clientSecret: '813caaea334a4fb5be54a8b9af3f4c97',
+};
 
 // prevent sending 'Origin' header, otherwise yandex doesn't execute an operation
 chrome.webRequest.onBeforeSendHeaders.addListener(({requestHeaders}) => ({
@@ -25,34 +31,41 @@ async function sendRequest(data) {
         .send(form)
         .query(query);
 
-    if (type === 'json' && res.body.code === errors.NOT_AUTHORIZED) {
-        throw new Error(errors.NOT_AUTHORIZED);
-    }
-    else if (type === 'xml') {
-        const {responseXML} = res.xhr;
+    // json
+    if (type === 'json') {
+        const resData = res.body || JSON.parse(res.text);
 
-        if (!responseXML) {
-            throw new Error(`No XML in the response: ${res.text}`);
+        if (resData.code === errors.NOT_AUTHORIZED) {
+            throw new Error(errors.NOT_AUTHORIZED);
         }
 
-        // sometimes Yandex sends "redirect" instead of result, so we need just follow that url in response
-        // check on "redirect_to" should be before "error", because "redirect_to" response contains "error" as well
-        const redirect = responseXML.querySelector('redirect_to');
-        if (redirect) {
-            return sendRequest({
-                ...data,
-                url: redirect.textContent,
-            });
-        }
-
-        const err = responseXML.querySelector('error');
-        if (err) {
-            const errText = err.textContent || err.getAttribute('code');
-            throw new Error(`Error in the response: ${errText}`);
-        }
+        return resData;
     }
 
-    return type === 'json' ? res.body : res.xhr.responseXML;
+    // xml
+    const {responseXML} = res.xhr;
+
+    if (!responseXML) {
+        throw new Error(`No XML in the response: ${res.text}`);
+    }
+
+    // sometimes Yandex sends "redirect" instead of result, so we need just follow that url in response
+    // check on "redirect_to" should be before "error", because "redirect_to" response contains "error" as well
+    const redirect = responseXML.querySelector('redirect_to');
+    if (redirect) {
+        return sendRequest({
+            ...data,
+            url: redirect.textContent,
+        });
+    }
+
+    const err = responseXML.querySelector('error');
+    if (err) {
+        const errText = err.textContent || err.getAttribute('code');
+        throw new Error(`Error in the response: ${errText}`);
+    }
+
+    return res.xhr.responseXML;
 }
 
 export async function getUser() {
@@ -77,13 +90,45 @@ export async function getMessagesCount() {
     return res.counters.unread;
 }
 
+async function getUid() {
+    const uid = await getCookieUid();
+    const res = await sendRequest({
+        url: AUTH_CONFIG.passportUrl,
+        query: {
+            yu: uid,
+        },
+    });
+
+    return res.default_uid;
+}
+
+async function getToken(uid) {
+    const sessionId = await getSessionId();
+
+    const res = await sendRequest({
+        method: 'post',
+        url: AUTH_CONFIG.tokenUrl,
+        form: {
+            grant_type: 'sessionid',
+            host: 'yandex.ru',
+            client_id: AUTH_CONFIG.clientId,
+            client_secret: AUTH_CONFIG.clientSecret,
+            sessionid: sessionId,
+            uid,
+        },
+    });
+
+    return res.access_token;
+}
+
 export async function getSocketCredentials() {
     const uid = await getUid();
+    const token = await getToken(uid);
 
-    return sendRequest({
-        url: `${DOMAIN}/neo2/handlers/xiva_sub.jsx`,
-        query: {req: uid},
-    });
+    return {
+        uid,
+        token,
+    };
 }
 
 export async function getMessages() {
