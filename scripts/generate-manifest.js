aeecdc68d469434046e379ddfa1bdd5459931bcc
Alexander Ivantsov <alexivantsov@ya.ru>
2017-05-24 19:13:24 +0200

    Add spaces to manifest files

--- scripts/generate-manifest.js
+++ scripts/generate-manifest.js
@@ -3,6 +3,6 @@ const getManifest = require('../src/manifest');
 
 const target = process.argv[2];
 
-fs.outputJsonSync('./dist/manifest.json', getManifest(target));
+fs.outputJsonSync('./dist/manifest.json', getManifest(target), {spaces: 2});
 
 console.log('Manifest is generated.');
