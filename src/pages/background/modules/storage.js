9dc5d5e9aba62c28ec112f2c9f2056cade27054d
Alexander Ivantsov <alexivantsov@ya.ru>
2017-05-31 23:01:45 +0200

    add storage module

---
+++ src/pages/background/modules/storage.js
@@ -0,0 +1,29 @@
+const chromeStore = chrome.storage.sync;
+
+export default {
+    getAll() {
+        return new Promise(resolve => {
+            chromeStore.get(null, data => {
+                const parsedData = Object.keys(data).reduce((obj, key) => {
+                    try {
+                        obj[key] = JSON.parse(data[key]);
+                    }
+                    catch(err){}
+
+                    return obj;
+                }, {});
+
+                resolve(parsedData);
+            });
+        });
+    },
+    set(name, value) {
+        chromeStore.set({[name]: JSON.stringify(value)});
+    },
+    remove(name) {
+        chromeStore.remove(name);
+    },
+    clear() {
+        chromeStore.clear();
+    }
+};
