7b032e86ba58c959ddd25c580eea4d18ddb9b47d
Alexander Ivantsov <alexivantsov@ya.ru>
2017-06-05 22:53:54 +0200

    Extract ext-store to a separate file to avoid circular dependencies

--- src/pages/background/redux/store.js
+++ src/pages/background/redux/store.js
@@ -1,31 +1,7 @@
 import {createStore, applyMiddleware} from 'redux';
-import {createBackgroundStore} from 'redux-webext';
 import initSubscriber from 'redux-subscriber';
-import {
-    LOAD_MESSAGES,
-    UPDATE_MESSAGE,
-} from 'shared/redux-consts/messages';
-import {
-    OPEN_LINK,
-    OPEN_SETTINGS,
-    RELOAD_APP,
-    OPEN_DONATION_LINK,
-} from 'shared/redux-consts/popup';
-import {UPDATE_SETTINGS} from 'shared/redux-consts/settings';
 import middlewares from './middlewares';
 import reducer from './reducers';
-import {
-    loadMessages,
-    updateMessage,
-    invalidateMessages,
-} from './actions/messages';
-import {
-    openLink,
-    openSettings,
-    reloadApp,
-    openDonationLink,
-} from './actions/popup';
-import {updateSettings} from './actions/settings';
 
 const store = createStore(
     reducer,
@@ -34,18 +10,4 @@ const store = createStore(
 
 initSubscriber(store);
 
-export default createBackgroundStore({
-    store,
-    actions: {
-        [LOAD_MESSAGES]: loadMessages,
-        [UPDATE_MESSAGE]: updateMessage,
-        [OPEN_LINK]: openLink,
-        [OPEN_SETTINGS]: openSettings,
-        [RELOAD_APP]: reloadApp,
-        [OPEN_DONATION_LINK]: openDonationLink,
-        [UPDATE_SETTINGS]: updateSettings,
-    },
-    onDisconnect() {
-        store.dispatch(invalidateMessages());
-    },
-});
+export default store;
