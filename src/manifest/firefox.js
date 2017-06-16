6cd7f1ff5a35ff66b664988ff5d1bd3e8e77596a
Alexander Ivantsov <alexivantsov@ya.ru>
2017-06-16 22:49:22 +0200

    update min firefox version to support storage.sync

--- src/manifest/firefox.js
+++ src/manifest/firefox.js
@@ -2,7 +2,7 @@ module.exports = {
     applications: {
         gecko: {
             id: 'jid0-vsitoD18LSTi4VQGGLCVmIoJSsU@jetpack',
-            strict_min_version: '52.0',
+            strict_min_version: '53.0',
         },
     },
     options_ui: {
